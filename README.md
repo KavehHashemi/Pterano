ddfaa18fa7dfbc5149d1e7c5773f8e13d066ca92
Kaveh <74491598+KavehHashemi@users.noreply.github.com>
2023-07-29 10:57:08 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a telegram bot written in TypeScript using Grammy and Node. The bot uses langChain to connect to OpenAI api and answer user's question based on a provided .txt file. It also has i18n for Farsi language.
+
+### > it is a work in progress
+### > it has dummy commands and inline keyboards
+### > you have to provide your own open api key and telegram bot token, and place them in a .env file
+### > as usual, clone the repo, npm i, npm start :)
---
+++ bot.ts
@@ -0,0 +1,76 @@
+import { Bot, InlineKeyboard, Keyboard, Context } from "grammy";
+import { I18n, I18nFlavor } from "@grammyjs/i18n";
+import dotenv from "dotenv";
+import commands from "./commands.json";
+import { QA } from "./qa";
+
+type MyContext = Context & I18nFlavor;
+
+dotenv.config();
+const Token: string = process.env.TOKEN || "";
+const bot = new Bot<MyContext>(Token);
+
+const i18n = new I18n<MyContext>({
+  defaultLocale: "fa",
+  directory: "locales",
+  globalTranslationContext: function g(ctx: Context) {
+    return {
+      name: ctx.from?.first_name || "",
+    };
+  },
+});
+bot.use(i18n);
+
+bot.api.setMyCommands(commands);
+
+const customInline = new InlineKeyboard()
+  .text("first option")
+  .row()
+  .text("second option")
+  .row()
+  .switchInline("third option");
+
+const productsKeyborad = new Keyboard()
+  .oneTime()
+  .text("فطیر")
+  .text("رب")
+  .row()
+  .text("لواشک")
+  .text("روغن")
+  .row();
+
+bot.command("start", async (ctx) => {
+  await ctx.reply(ctx.t("start"));
+});
+
+bot.command("products", (ctx) =>
+  ctx.reply("Displaying our products", { reply_markup: productsKeyborad })
+);
+
+bot.command("custom", (ctx) =>
+  ctx.reply("Custom Command", {
+    reply_markup: customInline,
+  })
+);
+
+bot.hears("روغن", (ctx) => {
+  ctx.reply("شما روغن را انتخاب کردید");
+});
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.on("message:text", async (ctx) => {
+  let answer = "";
+  answer = (await QA(ctx.message.text)).text;
+  try {
+    ctx.reply(answer);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+bot.start({
+  onStart: (u) => {
+    u.username;
+  },
+});
